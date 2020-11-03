95b1fd02190db7c1edeb5dcbfc6426515045d7a2
Alusbec <adrienlsbc@gmail.com>
2020-11-03 10:15:03 +0100

    exo4js

---
+++ Asset/Style.js
@@ -0,0 +1,9 @@
+document.getElementById('clickbutton').onclick=showAlert;
+function showAlert(){
+    var nom = document.getElementById("nom").value;
+    var prenom = document.getElementById("prenom").value;
+    var ville = document.getElementById("ville")
+
+    alert('bonjour'+ ' ' +nom+ ' ' +prenom);
+}
+                              
